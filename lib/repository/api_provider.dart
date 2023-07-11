a3a8d4f23a0edba4a99a826c949d6ea51b6a1ff7
islom <islomjon20010930@gmail.com>
2023-07-11 21:53:04 +0500

    some changes

--- lib/repository/api_provider.dart
+++ lib/repository/api_provider.dart
@@ -3,7 +3,7 @@ import 'package:pray_time/models/pray_time_model.dart';
 
 class ApiProvider {
   Dio dio = Dio();
-  Future<PrayTimeModel?> getTimes() async {
+  getTimes() async {
     String url = 'https://islomapi.uz/api/present/day?region=Toshkent';
     final response = await dio.get(url);
     if (response.statusCode == 200) {
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:pray_time/models/pray_time_model.dart';
+import 'package:pray_time/repository/api_provider.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ApiProvider provider = ApiProvider();
+  late final PrayTimeModel data;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    loadData();
+
+    super.initState();
+  }
+
+  loadData() async {
+    data = await provider.getTimes();
+    isLoading = false;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(data.region),
+                  Text(data.weekday),
+                  Text(data.date.toString()),
+                  Text(data.times.tongSaharlik),
+                  Text(data.times.quyosh),
+                  Text(data.times.peshin),
+                  Text(data.times.asr),
+                  Text(data.times.shomIftor),
+                  Text(data.times.hufton),
+                ],
+              ),
+            ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Column(children: [Row(children: [Text('region:'),],)],)
-        },
+        onPressed: () {},
         child: Icon(Icons.get_app),
       ),
     );
